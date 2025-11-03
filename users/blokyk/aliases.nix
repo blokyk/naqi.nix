2ed0dda011cfa3e34084fd5d3366c416b3231a12
blokyk <courvoisier.zoe@gmail.com>
2025-11-03 01:27:17 +0000

    nit: alias updates
    why did i use 'sudo true; sudo ...' ? i have no idea... o_o;

--- users/blokyk/aliases.nix
+++ users/blokyk/aliases.nix
@@ -1,4 +1,4 @@
 {
-    nom-switch = "sudo true; sudo nixos-rebuild switch --log-format internal-json -v 2>&1 |& nom --json";
-    nom-test = "sudo true; sudo nixos-rebuild test --log-format internal-json -v 2>&1 |& nom --json";
+    nom-switch = "sudo nixos-rebuild switch --log-format internal-json -v 2>&1 |& nom --json";
+    nom-test = "sudo nixos-rebuild test --log-format internal-json -v 2>&1 |& nom --json";
 }
