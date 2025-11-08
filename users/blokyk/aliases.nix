3c6375900008e894b2485597d61ed3865261c53b
blokyk <courvoisier.zoe@gmail.com>
2025-11-08 01:47:53 +0000

    nom: refactor aliases + add nom-upgrade

--- users/blokyk/aliases.nix
+++ users/blokyk/aliases.nix
@@ -1,4 +1,10 @@
-{
-    nom-switch = "sudo true; sudo nixos-rebuild switch --log-format internal-json -v 2>&1 |& nom --json";
-    nom-test = "sudo true; sudo nixos-rebuild test --log-format internal-json -v 2>&1 |& nom --json";
+let
+    # we use `sudo true` because piping `sudo nixos-rebuild`
+    # into `nom` hides the password prompt otherwise
+    nom-alias = cmd: 
+        "sudo true && { sudo nixos-rebuild ${cmd} --log-format internal-json -v 2>&1 |& nom --json } && exec $SHELL";
+in {
+    nom-switch = nom-alias "switch";
+    nom-upgrade = nom-alias "switch --upgrade";
+    nom-test = nom-alias "test";
 }
