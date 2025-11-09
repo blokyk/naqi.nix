8ed113fc111b3a9e1a1574bd2d997c5843de44a0
blokyk <courvoisier.zoe@gmail.com>
2025-11-09 03:12:58 +0000

    nit: trim trailing whitespace

--- users/blokyk/aliases.nix
+++ users/blokyk/aliases.nix
@@ -1,7 +1,7 @@
 let
     # we use `sudo true` because piping `sudo nixos-rebuild`
     # into `nom` hides the password prompt otherwise
-    nom-alias = cmd: 
+    nom-alias = cmd:
         "sudo true && { sudo nixos-rebuild ${cmd} --log-format internal-json -v 2>&1 |& nom --json } && exec $SHELL";
 in {
     nom-switch = nom-alias "switch";
