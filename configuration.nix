0ff258f36d37422c13a76bf013f6777452a63f0a
blokyk <courvoisier.zoe@gmail.com>
2025-11-11 21:11:59 +0000

    nit: set $EDITOR to nano for everyone

--- configuration.nix
+++ configuration.nix
@@ -41,6 +41,11 @@ in {
      keyMap = "fr";
   };
 
+  # global environment variables
+  environment.variables = {
+    EDITOR = "nano";
+  };
+
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
