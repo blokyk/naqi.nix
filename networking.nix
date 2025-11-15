fdd39080ab53d9a7e681175cbcd58997782cc4fa
blokyk <courvoisier.zoe@gmail.com>
2025-11-15 16:24:30 +0000

    feat: add ipv6 support
    idk how i did it, buuut it works so

--- networking.nix
+++ networking.nix
@@ -2,22 +2,36 @@
   # fixme: find out WHY ens3 stays on configuring (and thus blocks `wait-online` & boot/switch) even when its configured (maybe ipv4/6 dhcp/rad/ndp problems?)
   systemd.network.wait-online.enable = false;
 
+  systemd.network.networks."40-ens3" = {
+    networkConfig = {
+      IPv6AcceptRA = true;
+    };
+
+    routes = [
+      { Gateway = "2a0d:5940:112::1"; }
+    ];
+  };
+
   networking = {
     useNetworkd = true;
-    useDHCP = false;
+    useDHCP = true;
     dhcpcd.enable = false;
 
     enableIPv6 = true;
 
     interfaces.ens3 = {
-      ipv4.addresses = [{
-        address = "77.90.19.122";
-        prefixLength = 24;
-      }];
-      ipv6.addresses = [{
-        address = "2a0d:5940:112:2a::a";
-        prefixLength = 64;
-      }];
+      ipv4 = {
+        addresses = [{
+          address = "77.90.19.122";
+          prefixLength = 24;
+        }];
+      };
+      ipv6 = {
+        addresses = [{
+          address = "2a0d:5940:112:2a::a";
+          prefixLength = 64;
+        }];
+      };
     };
 
     defaultGateway = {
