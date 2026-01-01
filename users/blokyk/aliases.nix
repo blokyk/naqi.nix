0a96c8855964045b33ba3fadd1f4b8d92605d4ad
blokyk <courvoisier.zoe@gmail.com>
2026-01-01 15:16:55 +0000

    blokyk(alias/nom-*): --fast -> --no-reexec

--- users/blokyk/aliases.nix
+++ users/blokyk/aliases.nix
@@ -2,7 +2,7 @@ let
   # we use `sudo true` because piping `sudo nixos-rebuild`
   # into `nom` hides the password prompt otherwise
   nom-alias = cmd:
-    "sudo true && { sudo nixos-rebuild ${cmd} --fast --log-format internal-json -v 2>&1 |& nom --json } && exec $SHELL";
+    "sudo true && { sudo nixos-rebuild ${cmd} --no-reexec --log-format internal-json -v 2>&1 |& nom --json } && exec $SHELL";
 in {
   nom-switch = nom-alias "switch";
   nom-upgrade = nom-alias "switch --upgrade";
