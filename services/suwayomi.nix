b04289907fb0141ba35ac3fb3fc4f5d99bc86e85
blokyk <courvoisier.zoe@gmail.com>
2026-01-01 02:00:48 +0000

    suwayomi: 2.1.2031 -> 2.1.2038

--- services/suwayomi.nix
+++ services/suwayomi.nix
@@ -3,7 +3,7 @@ let
   suwayomi = config.services.suwayomi-server;
   passwdFile = config.sops.secrets.suwayomi.path;
 
-  version = "2.1.2031";
+  version = "2.1.2038";
 
   flare = config.services.flaresolverr;
 in {
@@ -16,7 +16,7 @@ in {
       # specifically fetch the jar artifact
       src = pkgs.fetchurl {
         url = "https://github.com/Suwayomi/Suwayomi-Server-Preview/releases/download/v${version}/Suwayomi-Server-v${version}.jar";
-        hash = "sha256-NY+PqWv3BG4F7nFJrWc4/Nx2KXu2oKlTEx5h/1WLapY=";
+        hash = "sha256-5Md+Kgy3Fa9sHF3aK6iMTOmpo+yIMI/qaXaUmvCYF2g=";
       };
     });
 
