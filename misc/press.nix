19d996c8f4a3c011b9c409f3de9653209afec507
blokyk <courvoisier.zoe@gmail.com>
2026-01-16 00:43:16 +0000

    press: use `fetchGit` to get RossSmyth/press

--- misc/press.nix
+++ misc/press.nix
@@ -1,9 +1,8 @@
 { ... }:
 let
-  press = builtins.fetchGit {
-    url = "https://github.com/RossSmyth/press";
-    rev = "2807d1ed2315f971ccc6c61501f6bc0aaa0cb1bf";
-    shallow = true;
+  press = fetchTarball {
+    url = "https://github.com/RossSmyth/press/archive/2807d1ed2315f971ccc6c61501f6bc0aaa0cb1bf.tar.gz";
+    sha256 = "sha256-MVYF/c1YXzS4BO0BkSVPKbBh69g2t0kniMq0oS10198=";
   };
 in {
   nixpkgs.overlays = [ (import press) ];
