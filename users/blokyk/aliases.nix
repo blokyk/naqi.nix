95f9ff4f16cdaacc3086ae0d86e4cd177a485a39
blokyk <courvoisier.zoe@gmail.com>
2026-01-20 05:22:34 +0000

    chore: fix nom-* aliases after npins stuff

--- users/blokyk/aliases.nix
+++ users/blokyk/aliases.nix
@@ -2,9 +2,21 @@ let
   # we use `sudo true` because piping `sudo nixos-rebuild`
   # into `nom` hides the password prompt otherwise
   nom-alias = cmd:
-    "sudo true && { sudo nixos-rebuild ${cmd} --no-reexec --log-format internal-json -v 2>&1 |& nom --json } && exec $SHELL";
+    ''
+      sudo true && {
+        sudo --preserve-env=NIX_PATH \
+          /etc/nixos/bootstrap.sh ${cmd} --no-reexec \
+            --log-format internal-json -v 2>&1 \
+          |& nom --json
+        } &&
+      exec sudo --login --user=$USER
+    '';
 in {
   nom-switch = nom-alias "switch";
-  nom-upgrade = nom-alias "switch --upgrade";
   nom-test = nom-alias "test";
+
+  # update the pins and then just do a normal switch
+  nom-upgrade = ''
+    npins -d /etc/nixos/npins update
+  '' + nom-alias "switch";
 }
